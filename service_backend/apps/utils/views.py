73a2aef0776015759329365a2c9ea0092d0b8eff
ccy <501900431@qq.com>
2023-04-13 20:33:41 +0800

    [feat](鉴权):封装鉴权代码至check_privilege装饰器, 修复UserCreate和UserLogin部分bug

--- service_backend/apps/utils/views.py
+++ service_backend/apps/utils/views.py
@@ -1,8 +1,11 @@
 from service_backend.apps.utils.constants import GlobalCode, UserErrorCode
+from service_backend.apps.users.models import Privilege
 from service_backend.settings import ENV
+from rest_framework.response import Response
 from datetime import datetime
 from jwt import encode, decode
 from hashlib import sha256
+from functools import wraps
 
 
 def response_json(success, code=None, message=None, data=None):
@@ -35,6 +38,8 @@ def decode_jwt(token: str) -> (int, dict):
     return user_id, response
 
 
+
+
 def generate_jwt(user_id: int) -> str:
     cur_time = str(datetime.now())
     token = encode(payload={'user_id': user_id, 'time': cur_time}, algorithm='HS256',
@@ -42,10 +47,54 @@ def generate_jwt(user_id: int) -> str:
     return token
 
 
+def check_jwt(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        token = args[1].data['jwt']  # args = (<class>, <request>)
+        print(type(token), token)
+        user_id, response = decode_jwt(token)
+        if not user_id:
+            return Response(response)
+        return f(*args, **kwargs, user_id=user_id)
+    return wrapper
+
+
+def check_privilege(role_list: list):
+    def decorated(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            # check jwt,  args = (<class>, <request>)
+            token = args[1].data['jwt']
+            user_id, response = decode_jwt(token)
+            if not user_id:
+                return Response(response)
+            # check authority
+            privilege = Privilege.objects.get(user_id=user_id)
+            if privilege.id and privilege.frozen:   # have privilege and not frozen
+                return Response(response_json(
+                    success=False,
+                    code=UserErrorCode.USER_FROZEN,
+                    message='your account has been frozen, please contact administrator!'
+                ))
+            # print(privilege.user_role, role_list)
+            if not privilege.id or not (privilege.user_role in role_list):
+                return Response(response_json(
+                    success=False,
+                    code=UserErrorCode.PERMISSION_DENIED,
+                    message='permission denied!'
+                ))
+            return f(*args, **kwargs, user_id=user_id)
+        return wrapper
+    return decorated
+
+
 def encode_password(message: str, salt=ENV['PASSWORD_SALT']) -> str:
     h = sha256()
     message += salt
     h.update(message.encode())
     for i in range(1000):
-        h.update(h.hexdigest())
+        h.update(h.hexdigest().encode())
     return h.hexdigest()
+
+
+# print(generate_jwt(10001))
