c70681dfd00789da565f211893b7746eb3508db3
CoolColoury <87513068+CoolColoury@users.noreply.github.com>
2023-04-18 13:46:10 +0800

    [fix](issue-API):代码编写完成,!未测试!

--- service_backend/apps/issues/serializers.py
+++ service_backend/apps/issues/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from service_backend.apps.issues.models import Issue
+from service_backend.apps.issues.models import Issue, Comment
 
-# TODO
+
 class IssueSerializer(serializers.ModelSerializer):
+    issue_id = serializers.SerializerMethodField()
     user_id = serializers.SerializerMethodField()
     user_name = serializers.SerializerMethodField()
     user_avatar = serializers.SerializerMethodField()
@@ -12,14 +13,26 @@ class IssueSerializer(serializers.ModelSerializer):
     subject_name = serializers.SerializerMethodField()
     tag_list = serializers.SerializerMethodField()
 
+    def get_issue_id(self, obj):
+        return obj.id
+
     def get_user_id(self, obj):
-        return obj.user.id
+        if obj.anonymous:
+            return 0
+        else:
+            return obj.user.id
 
     def get_user_name(self, obj):
-        return obj.user.name
+        if obj.anonymous:
+            return "匿名"
+        else:
+            return obj.user.name
 
     def get_user_avatar(self, obj):
-        return obj.user.avatar
+        if obj.anonymous:
+            return None
+        else:
+            return obj.user.avatar
 
     def get_chapter_id(self, obj):
         return obj.chapter.id
@@ -34,9 +47,102 @@ class IssueSerializer(serializers.ModelSerializer):
         return obj.chapter.subject.name
 
     def get_tag_list(self, obj):
-        return obj.issue_tags.tag
+        issue_tags = obj.issue_tags
+        tags_content = [issue_tag.tag.content for issue_tag in issue_tags]
+        return tags_content
 
     class Meta:
         model = Issue
-        fields = ['year_id', 'title', 'content', 'user_id', 'user_name', 'user_avatar', 'chapter_id', 'chapter_name',
-                  'subject_id', 'subject_name', 'tag_list', 'status', 'anonymous', 'created_at', 'updated_at']
+        fields = ['issue_id', 'title', 'content', 'user_id', 'user_name', 'user_avatar', 'chapter_id', 'chapter_name',
+                  'subject_id', 'subject_name', 'tag_list', 'status', 'anonymous', 'score', 'created_at', 'updated_at']
+
+
+class IssueSearchSerializer(serializers.ModelSerializer):
+    issue_id = serializers.SerializerMethodField()
+    user_id = serializers.SerializerMethodField()
+    user_name = serializers.SerializerMethodField()
+    user_avatar = serializers.SerializerMethodField()
+    chapter_id = serializers.SerializerMethodField()
+    chapter_name = serializers.SerializerMethodField()
+    subject_id = serializers.SerializerMethodField()
+    subject_name = serializers.SerializerMethodField()
+    issue_title = serializers.SerializerMethodField()
+
+    def get_issue_id(self, obj):
+        return obj.id
+
+    def get_issue_title(self, obj):
+        return obj.title
+
+    def get_user_id(self, obj):
+        if obj.anonymous:
+            return 0
+        else:
+            return obj.user.id
+
+    def get_user_name(self, obj):
+        if obj.anonymous:
+            return "匿名"
+        else:
+            return obj.user.name
+
+    def get_user_avatar(self, obj):
+        if obj.anonymous:
+            return None
+        else:
+            return obj.user.avatar
+
+    def get_chapter_id(self, obj):
+        return obj.chapter.id
+
+    def get_chapter_name(self, obj):
+        return obj.chapter.name
+
+    def get_subject_id(self, obj):
+        return obj.chapter.subject.id
+
+    def get_subject_name(self, obj):
+        return obj.chapter.subject.name
+
+    def get_counselor_id(self, obj):
+        return obj.counselor.id
+
+    def get_reviewer_id(self, obj):
+        return obj.reviewer.id
+
+    class Meta:
+        model = Issue
+        fields = ['issue_id', 'issue_title', 'content', 'user_id', 'user_name', 'user_avatar', 'chapter_id',
+                  'chapter_name', 'subject_id', 'subject_name', 'status', 'anonymous', 'score',
+                  'created_at', 'updated_at', 'counselor_id', 'reviewer_id']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    comment_id = serializers.SerializerMethodField()
+    time = serializers.SerializerMethodField()
+    user_id = serializers.SerializerMethodField()
+    user_role = serializers.SerializerMethodField()
+    avatar = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
+
+    def get_comment_id(self, obj):
+        return obj.id
+
+    def get_time(self, obj):
+        return obj.updated_at
+
+    def get_user_id(self, obj):
+        return obj.user.id
+
+    def get_user_role(self, obj):
+        return obj.user.user_role
+
+    def get_avatar(self, obj):
+        return obj.user.avatar
+
+    def get_name(self, obj):
+        return obj.user.name
+
+    class Meta:
+        model = Comment
+        fields = ['comment_id', 'content', 'time', 'user_id', 'user_role', 'avatar', 'name']
--- service_backend/apps/utils/constants.py
+++ service_backend/apps/utils/constants.py
@@ -9,6 +9,16 @@ class UserRole:
     ADMIN = 2
 
 
+# 问题状态，0：未认领(默认)，1：已认领，2：未认领复审 3: 已认领复审 4: 有效提问 5: 无效提问
+class IssueStatus:
+    NOT_ADOPT = 0
+    ADOPTING = 1
+    NOT_REVIEW = 2
+    REVIEWING = 3
+    VALID_ISSUE = 4
+    INVALID_ISSUE = 5
+
+
 class YearErrorCode:
     YEAR_SAVE_FAILED = 101
     YEAR_DOES_NOT_EXIST = 102
@@ -23,12 +33,6 @@ class UserErrorCode:
     USER_SAVE_FAILED = 205
     USER_FROZEN = 206
     PERMISSION_DENIED = 207
-
-
-class IssueErrorCode:
-    ISSUE_NOT_FOUND = 301
-    ISSUE_DELETE_FAILED = 302
-    ISSUE_SAVED_FAILED = 303
 
 
 class OtherErrorCode:
@@ -51,3 +55,33 @@ class TagErrorCode:
     TAG_SAVE_FAILED = 501
     TAG_DOES_NOT_EXIST = 502
     TAG_DELETE_FAILED = 503
+
+
+class IssueErrorCode:
+    ISSUE_NOT_FOUND = 601
+    ISSUE_DELETE_FAILED = 602
+    ISSUE_SAVED_FAILED = 603
+
+
+class CommentErrorCode:
+    COMMENT_NOT_FOUND = 601
+    COMMENT_DELETE_FAILED = 602
+    COMMENT_SAVED_FAILED = 603
+
+
+class IssueLikeErrorCode:
+    ISSUE_LIKE_SAVED_FAILED = 701
+    ISSUE_LIKE_DELETE_FAILED = 702
+
+
+class IssueFollowErrorCode:
+    ISSUE_FOLLOW_SAVED_FAILED = 701
+    ISSUE_FOLLOW_DELETE_FAILED = 702
+
+
+class IssueTagErrorCode:
+    ISSUE_TAG_SAVED_FAILED = 801
+    ISSUE_TAG_DELETE_FAILED = 802
+
+class IssueReviewerErrorCode:
+    REVIEWER_ISSUE_SAVED_FAILED = 901
