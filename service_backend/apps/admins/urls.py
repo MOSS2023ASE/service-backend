af4855aa7847f7fd9a6b062ecaefa879ebcf6c61
ubuntu <ubuntu@localhost.localdomain>
2023-04-20 12:39:16 +0800

    [fix](API):修复admins部分url的bug

--- service_backend/apps/admins/urls.py
+++ service_backend/apps/admins/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('freeze_user', views.FreezeUser.as_view()),
     path('issue/delete', views.DeleteIssue.as_view()),
     path('create_user', views.CreateUser.as_view()),
-    path('create_user_batch', views.CreateUser.as_view()),
+    path('create_user_batch', views.CreateUserBatch.as_view()),
 ]
